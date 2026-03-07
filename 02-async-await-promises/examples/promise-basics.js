3a371b3f603e08427fff513292d2ce3a1a303cfa
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-03-07 22:36:11 +0530

    added promise basics

--- 02-async-await-promises/examples/promise-basics.js
+++ 02-async-await-promises/examples/promise-basics.js
@@ -1,3 +1,4 @@
+// example 1
 //creating a promise
 
 const myPromise= new Promise((resolve,reject)=>{
@@ -15,14 +16,33 @@ console.log('myPromise',myPromise) //Promise {'done'}
 
 
 
-
+// example 2
 //creating a resolved promise use //Promise.resolve()
 const myPromise1= Promise.resolve().then(console.log('done'));
 console.log(myPromise1) //{<pending>} // because after printing done console.log() returns undefined.
 
 
+// Note:
+// What happens step-by-step
+
+// console.log('done') runs immediately (synchronous).
+
+// It prints:
+
+// done
+
+// console.log() returns undefined.
+
+// So the code effectively becomes:
+
+// Promise.resolve().then(undefined)
+
+// .then() receives no callback.
+
+// It still returns a new promise, which is initially pending until the microtask finishes.
 
 
+// example 3
 //creating a resolved promise use //Promise.resolve() 
 const myPromise2= Promise.resolve().then(()=>{console.log('done')})
 console.log(myPromise2);//{<pending>}
@@ -49,3 +69,13 @@ console.log(myPromise2);//{<pending>}
 // Promise { fulfilled: undefined }
 
 // The value is undefined because the .then() function doesn't return anything.
+
+// example 4
+//Handling a Promise
+myPromise.then((result)=>{
+  console.log('resolved', result);
+}).catch((result)=>{
+  console.log('rejected', result);
+})
+
+
