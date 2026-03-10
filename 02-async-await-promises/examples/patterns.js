f39e93b9f737c0f738b7fd683dd86b22d8205269
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-03-10 12:40:27 +0530

    added patterns of promises

--- 02-async-await-promises/examples/patterns.js
+++ 02-async-await-promises/examples/patterns.js
@@ -1,21 +1,65 @@
-// 1.Sequential Execution
+// Exercise 1
 
-// Tasks run one after another.
-
-async function sequential(){
-    const a = await fetchDataA();
-    const b = await fetchDataB();
+// Create two async functions with setTimeout.
+async function timer1(){
+    await new Promise((res)=>{
+        setTimeout(()=>{
+            res('done-1')
+        },1000)
+    }).then(console.log)
 }
 
-// 2. Parallel Execution
+async function timer2(){
+    await new Promise((res)=>{
+        setTimeout(()=>{
+            res('done-2')
+        },2000)
+    }).then(console.log)
+}
 
-// Runs tasks at the same time.
 
-async function parallel(){
-    const [a,b]=Promise.all(
-        [
-            await fetchDataA(),
-            await fetchDataB()
-        ]
-    )
-}//much faster
+timer1();
+timer2();
+
+
+// Exercise 2
+
+// Run them sequentially.
+async function timerNew() {
+     await new Promise((res)=>{
+        setTimeout(()=>{
+            res('done-1')
+        },1000)
+    }).then(console.log)
+     await new Promise((res)=>{
+        setTimeout(()=>{
+            res('done-2')
+        },2000)
+    }).then(console.log)
+}
+
+
+timerNew();
+
+// Exercise 3
+
+// Run them using Promise.all.
+
+async function timerNew1() {
+Promise.all([
+     await new Promise((res)=>{
+        setTimeout(()=>{
+            res('done-1')
+        },1000)
+    }).then(console.log),
+     await new Promise((res)=>{
+        setTimeout(()=>{
+            res('done-2')
+        },2000)
+    }).then(console.log)
+]);
+}
+
+timerNew1();
+
+// Compare execution time.
