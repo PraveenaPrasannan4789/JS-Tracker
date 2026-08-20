2738c2dbba211f6f066d03ac48c28b331015b13e
Praveena Prasannan <49147219+PraveenaPrasannan4789@users.noreply.github.com>
2026-08-20 22:38:34 +0100

    some array method example added

--- 03-array-methods/methods/some.js
+++ 03-array-methods/methods/some.js
@@ -2,12 +2,17 @@
 
 //check odd number exist
 
-const oddNumbers=[3,7,8]
-const check= oddNumbers.some((x)=>{
-    return x%3 === 0;
-})
-const checkEvery=oddNumbers.every((x)=>{
-    return x%3 ===0;
-})
-console.log('odd Number Exists',check);
-console.log('is all odd numbers',checkEvery);
+const oddNumbers = [3, 7, 8];
+const check = oddNumbers.some((x) => {
+  return x % 3 === 0;
+});
+const checkEvery = oddNumbers.every((x) => {
+  return x % 3 === 0;
+});
+console.log("odd Number Exists", check);
+console.log("is all odd numbers", checkEvery);
+
+const evenNumbers = [3, 7, 8];
+const check2 = evenNumbers.some((x) => {
+  return x % 2 === 0;
+});
